ea2b270aef13f112200592430c076f385fc7b764
vanbumi <jsp.dio@gmail.com>
2017-07-10 13:46:37 +0700

    end 13 wrap up input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,16 +4,17 @@ import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            placeholder="user@gmail.com"
             label="Email"
-            value={ this.state.text } 
-            onChangeText={ text => this.setState({ text }) }
+            value={ this.state.email } 
+            onChangeText={ email => this.setState({ email }) }
             //style={{ height: 20, width: 100 }}   
           />
         </CardSection>
